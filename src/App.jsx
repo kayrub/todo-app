8bfb27aff62fa719c6ed073966f999dcd81be5dc
Kaleb Lee <kayrub@Kalebs-MacBook-Pro.local>
2026-04-13 18:35:44 -0700

    feat add todo input, button, and list rendering

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,39 @@ import { useState } from 'react'
 
 function App() {
 
+  const [todoText, setToDoText] = useState("");
+  const [todos, setToDos] = useState(["temp1", "temp2", "temp3"]);
+
+
+  const updateToDo = (newToDo) => {
+    setToDos([...todos, newToDo]);
+    console.log("todo added:", newToDo);
+    console.log("todos list:", todos);
+  }
+
   return (
     <>
+      <header>
+        TEST HEADER
+      </header>
       <section>
         kayrub todos
       </section>
+      <div>
+        <input
+          id = "text-input"
+          type = "text"
+          value = {todoText}
+          onChange = {(e) => setToDoText(e.target.value)}
+          placeholder = "Please type here..."
+          />
+        <button onClick = {() => updateToDo(todoText)}>
+          Update List
+        </button>
+        <p>
+          {todos}
+        </p>
+      </div>
   
     </>
   )
