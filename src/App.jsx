b05f8c3d1db3425851ae292dd9bd2048fd60571d
Kaleb Lee <kayrub@Kalebs-MacBook-Pro.local>
2026-04-14 09:32:44 -0700

    refactor: convert todos to objects; feat: add Enter key submission

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
 import { useState } from 'react'
 
+/* Day 3 (25 min): Add a delete button next to each todo. Add a checkbox to mark todos as done (strikethrough text). Commit and push. 
+
+first change data set to be array of objects. in case duplicate list items.
+2nd map array of objects only display name
+3rd create delete handler using filter
+4th add delete button
+5th research strikethrough
+  input type =checkbox
+  create handler to check 
+  onChange boolean to check
+  css span style textDecoration, color
+15 minutes to research
+Blockers
+1st how to handle unique id, having an id counter
+25 min to change todos data to array of obj and to display
+
+
+*/
+
 function App() {
 
   const [todoText, setToDoText] = useState("");
-  const [todos, setToDos] = useState(["temp1", "temp2", "temp3"]);
+  const [todos, setToDos] = useState([{id: 1, name: "eggstest",}, {id: 2, name: "meattest"}]);
+  const [nextId, setNextId] = useState(3);
 
 
   const updateToDo = (newToDo) => {
-    setToDos([...todos, newToDo]);
+    setToDos(prev => [...prev, {id: nextId, name: newToDo}]);
+    setNextId(prev => prev + 1);  
     console.log("todo added:", newToDo);
     console.log("todos list:", todos);
+    console.log("current nextid:", nextId);
+  }
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      updateToDo(e.target.value);
+    }
   }
 
   return (
@@ -27,13 +55,16 @@ function App() {
           value = {todoText}
           onChange = {(e) => setToDoText(e.target.value)}
           placeholder = "Please type here..."
+          onKeyDown = {handleKeyDown}
           />
         <button onClick = {() => updateToDo(todoText)}>
           Update List
         </button>
-        <p>
-          {todos}
-        </p>
+        <ul>
+          {todos.map(todo => (
+            <li key={todo.id}>{todo.name}</li>
+          ))}
+        </ul>
       </div>
   
     </>
